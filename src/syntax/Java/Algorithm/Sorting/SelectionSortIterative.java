571b1d770eca474577fa13b8a90cc0c0c2943dc3
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2023-06-20 18:20:28 -0400

    selection sort iterative

---
+++ src/syntax/Java/Algorithm/Sorting/SelectionSortIterative.java
@@ -0,0 +1,34 @@
+package syntax.Java.Algorithm.Sorting;
+
+import java.util.Arrays;
+
+public class SelectionSortIterative {
+
+    public static void main(String[] args) {
+        int[] arr = {-55, 95, 21, -9, 3};
+
+
+        System.out.println(Arrays.toString(selectionSort(arr)));
+    }
+
+    private static int[] selectionSort(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+
+            int minInd = i;
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[j] < arr[minInd]) {
+
+                    minInd = j;
+                }
+            }
+
+            int temp = arr[minInd];
+            arr[minInd] = arr[i];
+            arr[i] = temp;
+
+        }
+        return arr;
+    }
+}
