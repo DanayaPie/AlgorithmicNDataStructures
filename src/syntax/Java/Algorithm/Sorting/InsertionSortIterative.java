e3094882d8c7803fc573cdddb861c2c910da7201
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2023-06-21 15:16:54 -0400

    insertion sort iterative

--- src/syntax/Java/Algorithm/Sorting/InsertionSortIterative.java
+++ src/syntax/Java/Algorithm/Sorting/InsertionSortIterative.java
@@ -1,17 +1,17 @@
 package syntax.Java.Algorithm.Sorting;
 
+import java.util.Arrays;
+
 public class InsertionSortIterative {
     public static void main(String[] args) {
         int[] arr = {18, 9, -11, 0};
 
-        arr = insertionSort(arr);
+        insertionSort(arr);
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
+        System.out.println(Arrays.toString(arr));
     }
 
-    private static int[] insertionSort(int[] arr) {
+    private static void insertionSort(int[] arr) {
 
         for (int i = 1; i < arr.length; i++) {
             int key = arr[i]; // current
@@ -24,6 +24,5 @@ public class InsertionSortIterative {
             }
             arr[j + 1] = key;
         }
-        return arr;
     }
 }
